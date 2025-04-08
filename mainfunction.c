b53b546bd4eb148b8b0d5a95bea3b3e5a15883cf
Hrithik <hrithikkesargopp@gmail.com>
2025-04-08 18:27:41 +0530

    Main Function

---
+++ mainfunction.c
@@ -0,0 +1,18 @@
+int main() {
+    int n;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("Element [%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    findMaxMin(arr, n);
+
+    return 0;
+}
